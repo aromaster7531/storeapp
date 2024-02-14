6316bc8f000b5bba46331a1ad88ab29ac718518b
Brian <briantran0930@gmail.com>
2024-02-14 01:20:22 -0800

    Changed Testing

--- test_app.py
+++ test_app.py
@@ -1,9 +1,13 @@
 import requests
+import pytest
+from app import app  # assuming your Bottle app is in my_bottle_app.py
+
 
 def test_welcome():
     response = requests.get('http://localhost:8080/')
     assert response.status_code == 200
     assert 'Welcome to DangerWay Market' in response.text
+
 
 def test_show_list():
     response = requests.get('http://localhost:8080/list')
@@ -13,3 +17,9 @@ def test_show_list():
     assert 'Bagel' in response.text
     assert 'Protein Powder' in response.text
     assert 'Cookie Dough' in response.text
+    
+def test_routes():
+    routes = ['/', '/list', '/deals', '/checkout/Milk/Lucrene Fat Free']
+    for route in routes:
+        response = requests.get(f'http://localhost:8080{route}')
+        assert response.status_code == 200
